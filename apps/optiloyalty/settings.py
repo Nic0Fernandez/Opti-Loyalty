e8e773e48813af837f505212acdf652baf6eb54e
FERNANDEZ Nicolas <nicolas.fernandez@telecomnancy.eu>
2024-11-19 09:46:18 +0100

    structure change

--- apps/optiloyalty/settings.py
+++ apps/optiloyalty/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent   #changer niveau de parent en fonction de la DB
 
 
 # Quick-start development settings - unsuitable for production
