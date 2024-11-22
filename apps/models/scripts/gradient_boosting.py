d35e9923e1132167995f7e6a0317eb317d06edb3
FERNANDEZ Nicolas <nicolas.fernandez@telecomnancy.eu>
2024-11-22 11:51:44 +0100

    change n_estimator

--- apps/models/scripts/gradient_boosting.py
+++ apps/models/scripts/gradient_boosting.py
@@ -24,7 +24,7 @@ def get_boosting_recommendations(client_id, db_path):
 
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
-    model = GradientBoostingClassifier(n_estimators=10, learning_rate=0.2, random_state=42, max_depth=3, subsample=0.8)
+    model = GradientBoostingClassifier(n_estimators=30, learning_rate=0.2, random_state=42, max_depth=3, subsample=0.8)
     model.fit(X_train, y_train)
 
 
